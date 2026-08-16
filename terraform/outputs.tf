b00ef5c2b1ff8a179eb0d8b1978c7a3eeae16c40
Jerome Christian Ibon <jeromevillanuevaibon97@gmail.com>
2026-08-16 14:00:10 +0800

    expose observability outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,15 @@ output "function_app_name" {
 output "function_app_default_hostname" {
   value = azurerm_linux_function_app.function.default_hostname
 }
+
+output "application_insights_name" {
+  value = azurerm_application_insights.portfolio.name
+}
+
+output "log_analytics_workspace_name" {
+  value = azurerm_log_analytics_workspace.portfolio.name
+}
+
+output "health_endpoint" {
+  value = "https://${azurerm_linux_function_app.function.default_hostname}/api/health"
+}
